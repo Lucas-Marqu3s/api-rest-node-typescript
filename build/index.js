b25a8981cb6dc58ec89ae82724555c1ca9842511
Lucas Marques <lucasses10@gmail.com>
2024-05-06 15:10:12 -0300

    ft: doing all CRUD tests with Jest

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-server_1.server.listen(process.env.PORT || 3333, () => {
+const Server_1 = require("./server/Server");
+Server_1.server.listen(process.env.PORT || 3333, () => {
     console.log(`App rodando na porta ${process.env.PORT || 3333}`);
 });
